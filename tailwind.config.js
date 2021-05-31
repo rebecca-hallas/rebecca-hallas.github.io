72a1c1082baf70fdd7ed7bcacdd010f2a56b482a
Rebecca-Hallas <rebeccajhallas@gmail.com>
2021-05-31 18:22:57 +1200

    begin rebuilding portfolio in static version

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      'custom': '"Press Start 2P"',
+      'customtwo': '"VT323"'
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
