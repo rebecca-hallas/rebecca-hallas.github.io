8cc214976eeafad11bfe5f515360057c42a5f011
Rebecca-Hallas <rebeccajhallas@gmail.com>
2021-06-12 20:16:10 +1200

    fix tailwind config file so bg hover works

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,24 +2,26 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    fill: theme => ({
-      'red': theme('colors.red.500'),
-      'green': theme('colors.green.500'),
-      'blue': theme('colors.blue.500'),
-    }),
-    screens: {
-      'lg': '1025px',
-      'ml': '1024px'
-    },
-    fontFamily: {
-      'custom': '"Press Start 2P"',
-      'customtwo': '"VT323"'
-    },
     extend: {
       backgroundImage: theme => ({
         'space': "url('./images/retro.png')",
-        'game': "url('./images/game.png')"
+        'game': "url('./images/game.png')",
+        'cat': "url('./images/catlol.png')"
       }),
+
+      fill: theme => ({
+        'red': theme('colors.red.500'),
+        'green': theme('colors.green.500'),
+        'blue': theme('colors.blue.500'),
+      }),
+      screens: {
+        'lg': '1025px',
+        'ml': '1024px'
+      },
+      fontFamily: {
+        'custom': '"Press Start 2P"',
+        'customtwo': '"VT323"'
+      },
       colors: {
         hotPink: '#ff6bd5',
         limeGreen: '#52ff4d'
@@ -29,8 +31,8 @@ module.exports = {
           '0%, 100%': { transform: 'rotate(-3deg)' },
           '50%': { transform: 'rotate(3deg)' },
         }
-       },
-       animation: {
+      },
+      animation: {
         wiggle: 'wiggle 1s ease-in-out infinite',
         'bounce-slow': 'bounce 2s linear infinite',
         'spin-slow': 'spin 4s linear infinite',
@@ -39,15 +41,15 @@ module.exports = {
         '270': '270deg',
         '330': '330deg',
         '360': '360deg',
+      },
+    },
   },
   variants: {
     extend: {
-        backgroundImage: ['hover', 'focus'],
-        animation: ['hover', 'focus'],
-        fill: ['hover', 'focus']
-      }
-    },
+      backgroundImage: ['hover', 'focus'],
+      animation: ['hover', 'focus'],
+      fill: ['hover', 'focus']
+    }
   },
   plugins: [],
-}
 }
